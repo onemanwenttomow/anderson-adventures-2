8f58560f70add8e6189200046fccf561bdc3603f
Peter Anderson <pete.anderson@gmail.com>
2021-12-02 20:49:21 +0100

    --add: started working on games page

--- pages/games.vue
+++ pages/games.vue
@@ -1,5 +1,21 @@
+<script setup lang="ts">
+const games = ref([{ name: "TimesTables" }]);
+</script>
+
 <template>
-  <AppPageWrapper class="flex justify-center items-center">
-    <h1>GAMES!</h1>
-  </AppPageWrapper>
+    <AppPageWrapper class="px-4">
+        <div class="pt-6">
+            <div v-for="game in games" :key="game.name" class="py-4">
+                <AppPixelCanvas
+                    src="/items/Weapon_08.png"
+                    :size="5"
+                    @imgloaded="true"
+                />
+
+                <h4>
+                    {{ game.name }}
+                </h4>
+            </div>
+        </div>
+    </AppPageWrapper>
 </template>
