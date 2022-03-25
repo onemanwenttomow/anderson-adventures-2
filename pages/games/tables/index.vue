acf41f5fa2f90eb9c64190b893b1c0331bd8eb68
Peter Anderson <pete.anderson@gmail.com>
2022-03-25 20:36:58 +0100

    --add: transition play

--- pages/games/tables/index.vue
+++ pages/games/tables/index.vue
@@ -122,7 +122,9 @@ function handleInput() {
     >
       <div>{{ currentTimesTable }}</div>
       <div>&middot;</div>
-      <div>{{ randomQuestion }}</div>
+      <transition name="slide-fade" mode="out-in">
+        <div :key="randomQuestion">{{ randomQuestion }}</div>
+      </transition>
       <div>=</div>
       <input
         v-model="userInput"
@@ -196,6 +198,18 @@ function handleInput() {
 .shake {
   animation: shake v-bind(damageTimeMs);
   animation-iteration-count: infinite;
+}
+
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active for <2.1.8 */ {
+  transform: translateY(10px);
+  opacity: 0;
 }
 
 @keyframes shake {
