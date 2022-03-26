3cc583ffe41e87289b0724ba60015c869608c4eb
Peter Anderson <pete.anderson@gmail.com>
2022-03-26 11:40:02 +0100

    --refac: defeated monsters by level

--- pages/games/defeated/index.vue
+++ pages/games/defeated/index.vue
@@ -7,24 +7,26 @@ const { playerSelected } = usePlayersStore();
 const { $supabase } = useNuxtApp();
 const { getDefetatedMonsters } = $supabase;
 const data = await getDefetatedMonsters(playerSelected.id);
+console.log("monsters: ", monsters);
 
 const alreadyDefeated = ref(data.map((row) => row.monster_id));
-
-const flatMonsters = monsters.flat();
 </script>
 
 <template>
   <AppPageWrapper class="px-4">
-    <div class="flex flex-wrap justify-center">
-      <AppUserIcon
-        v-for="monster in flatMonsters"
-        :key="monster.id"
-        :character="monster.imgUrl"
-        :id="monster.id"
-        height="h-30"
-        width="w-30"
-        :class="alreadyDefeated.includes(Number(monster.id)) ? '' : 'grayed'"
-      />
+    <div v-for="(level, i) in monsters">
+      <h3 class="text-2xl">Level {{ i + 1 }}</h3>
+      <div class="flex flex-wrap">
+        <AppUserIcon
+          v-for="monster in level"
+          :key="monster.id"
+          :character="monster.imgUrl"
+          :id="monster.id"
+          height="h-30"
+          width="w-30"
+          :class="alreadyDefeated.includes(Number(monster.id)) ? '' : 'grayed'"
+        />
+      </div>
     </div>
   </AppPageWrapper>
 </template>
