afda46a9e0a91c702e02b7a921ff6b1de8479278
Peter Anderson <pete.anderson@gmail.com>
2022-03-26 13:35:11 +0100

    --add: theme colour

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+useMeta({
+  meta: [
+    {
+      name: "theme-color",
+      content: "#111827"
+    }
+  ]
+});
+</script>
+
 <template>
   <div class="h-full background">
     <AppHeader />
