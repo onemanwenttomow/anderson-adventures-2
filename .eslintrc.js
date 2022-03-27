ed9350f0c2e7b6aba9dd5d1cee807fd7d868071d
Peter Anderson <pete.anderson@gmail.com>
2022-03-27 14:00:59 +0200

    --add: defeated monsters at end of game link to page

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,38 @@
-// module.exports = {
-// 	indent: ["error", 2]
-// };
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended',
+    'prettier',
+  ],
+  plugins: [],
+  // add your custom rules here
+  rules: {
+    'no-console': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/html-self-closing': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/no-unused-components': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'vue/no-v-html': 'off',
+    'no-undef': 'off',
+
+    // temps
+    eqeqeq: 'off',
+    'no-lone-blocks': 'off',
+    'no-unused-expressions': 'off',
+    //
+
+    camelcase: 'off',
+    'vue/valid-v-slot': [
+      'error',
+      {
+        allowModifiers: true,
+      },
+    ],
+  },
+};
